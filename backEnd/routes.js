41a77222dfff7585dd3e4817ad8ff4a2f0c514da
Georgi <georgi.nikolow@gmail.com>
2019-09-07 13:19:47 -0400

    changed object properties name

--- backEnd/routes.js
+++ backEnd/routes.js
@@ -1,27 +1,27 @@
 module.exports = {
     createReservation: function (req, res) {
-        var arr1 = [];
-        var arr2 = []
-        var Customers = function (name, phoneN, time, ID) {
+        var seated  = [];
+        var waiting = []
+        var Customers = function (name, phone, email, id) {
             this.name = name;
-            this.phoneN = phoneN;
-            this.time = time;
-            this.ID = ID;
+            this.phone = phone;
+            this.email = email;
+            this.id = id;
         };
 
-        if (arr1.legnth <= 5) {
-            for (var i = 0; i < arr1.length; i++) {
-                arr1.push(new Customers(this.name));
+        if (seated.legnth <= 5) {
+            for (var i = 0; i < seated.length; i++) {
+                seated.push(new Customers(this.name));
             }
         } else {
-            arr2.push(new Customers(req.body.name, req.body.phoneN, req.body.time, req.body.ID))
+            waiting.push(new Customers(req.body.name, req.body.phone, req.body.email, req.body.id))
         
     };
     res.json(Customers);
 },
 
     GetTables: function (req, res){
-        
+
     }
 
     
